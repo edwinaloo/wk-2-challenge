4526e46c4cf22fdeed2a2200caafd90c68ca4b27
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-28 13:59:59 +0300

    update BotCollection.js

---
+++ bot-army-app/src/components/BotCollection.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const BotCollection = ({ addToArmy }) => {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    const fetchBots = async () => {
+      try {
+        const response = await fetch('http://localhost:8001/bots');
+        const data = await response.json();
+        setBots(data);
+      } catch (error) {
+        console.error('Error fetching bots:', error);
+      }
+    };
+
+    fetchBots();
+  }, []);
+
+  const handleBotSelection = (bot) => {
+    addToArmy(bot);
+  };
+
+  return (
+    <div>
+      <h2>Bot Collection</h2>
+      <div>
+        {bots.map((bot) => (
+          <div key={bot.id}>
+            <h3>{bot.name}</h3>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <button onClick={() => handleBotSelection(bot)}>Select</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BotCollection;
