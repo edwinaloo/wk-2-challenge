0446bba4b681b852fc16b12b7db51e847ee8de50
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-28 14:46:20 +0300

    ~push App.js changes

--- bot-army-app/src/App.js
+++ bot-army-app/src/App.js
@@ -1,30 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BotCollection from './components/BotCollection';
 import YourBotArmy from './components/YourBotArmy';
 
 function App() {
+  const [army, setArmy] = useState([]);
+
+  const addToArmy = (bot) => {
+    // Check if the bot is already in the army
+    const isBotInArmy = army.some((b) => b.id === bot.id);
+    if (!isBotInArmy) {
+      setArmy([...army, bot]);
+    }
+  };
+
+  const releaseFromArmy = (bot) => {
+    const updatedArmy = army.filter((b) => b.id !== bot.id);
+    setArmy(updatedArmy);
+  };
+
+  const dischargeBot = async (bot) => {
+    try {
+      // Delete the bot from the backend
+      await axios.delete(`http://localhost:8001/bots/${bot.id}`);
+      // Remove the bot from the army state
+      releaseFromArmy(bot);
+    } catch (error) {
+      console.error('Error discharging bot:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <BotCollection />
-      <YourBotArmy />
+    <div>
+      <BotCollection onAddToArmy={addToArmy} />
+      <YourBotArmy
+        army={army}
+        onReleaseFromArmy={releaseFromArmy}
+        onDischarge={dischargeBot}
+      />
     </div>
   );
 }
+
 
 export default App;
